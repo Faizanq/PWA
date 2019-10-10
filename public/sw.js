ca58eb11feabb5e74e582a2cdf43a30262a06896
Faizan <faizanqureshi0@gmail.com>
2019-10-11 00:02:31 +0530

    cache.add done

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,14 @@
 self.addEventListener('install',(event)=>{
 	console.log('[Service worker]','Installing serviceWorker',event);
+	event.waitUntil(
+		caches.open('static')
+		.then(function(cache){
+			console.log('[Service worker] prefetching shell');
+			cache.add('/')
+			cache.add('/index.html')
+			cache.add('/src/js/app.js')
+		})
+		);
 })
 
 self.addEventListener('activate',(event)=>{
@@ -8,6 +17,15 @@ self.addEventListener('activate',(event)=>{
 });
 
 self.addEventListener('fetch',(event)=>{
-	console.log('[Service worker]','Going through fetch event',event);
-	// event.respondWith(fetch(event.request));
+	// console.log('[Service worker]','Going through fetch event',event);
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response){
+			if(response){
+				return response;
+			}else{
+				return fetch(event.request)
+			}
+		})
+	);
 })
